4c980d333ec27ce877a57d608aad1480d4253426
VillodresMorenoRaul <al.raul.villodres.moreno@iesportada.org>
2021-02-28 16:10:55 +0100

    Estructura_Proyecto2

---
+++ PMDM_TO03_Raul_Villodres/app/src/main/java/com/example/pmdm_to03_raul_villodres/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.pmdm_to03_raul_villodres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pmdm_to03_raul_villodres.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActivityMainBinding binding;
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.AccederEjercicio1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AbrirActividad1();
+            }
+        });
+
+        binding.AccederEjercicio2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AbrirActividad2();
+            }
+        });
+
+        binding.AccederEjercicio3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AbrirActividad3();
+            }
+        });
+
+        binding.AccederEjercicio4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AbrirActividad4();
+            }
+        });
+    }
+
+
+    //Método AbrirActividad1(), que abrirá la clase asignada al ejercicio 1
+
+    public void AbrirActividad1() {
+        Intent intent = new Intent(this, MainActivity1.class);
+        startActivity(intent);
+    }
+
+    public void AbrirActividad2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void AbrirActividad3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+
+    public void AbrirActividad4() {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+
+}
